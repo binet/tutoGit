538f3a1d62bd34dc8c820cdae3dfdc5f6557855a
binet <bonnet.st30@hotmail.fr>
2018-10-29 22:56:05 +0100

    Ajout du fichier JS

---
+++ main.js
@@ -0,0 +1,35 @@
+window.onload = function()
+{
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var x = 0;
+    var y = 0;
+    
+    
+    init();
+    
+    function init()
+    {
+    canvas = document.createElement ('canvas');
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border = "1px solid";
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+    refreshCanvas();
+    
+    }
+    
+    function refreshCanvas()
+    {
+        x += 5;
+        y += 2;
+     ctx.clearRect(0,0,canvas.width, canvas.height);  
+     ctx.fillStyle = "#ff0000";
+     ctx.fillRect(x,y,100,50); 
+     setTimeout(refreshCanvas,delay);
+    }
+	
+	
+}
